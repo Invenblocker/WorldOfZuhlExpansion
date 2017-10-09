0b24ac6a13b7ab729dbb64822520b4a0844da6ba
Invenblocker <32257137+Invenblocker@users.noreply.github.com>
2017-10-09 10:39:31 +0200

    Tilføjede getInvetory()

--- Slower Than Light/src/logic/elements/characters/Player.java
+++ Slower Than Light/src/logic/elements/characters/Player.java
@@ -10,5 +10,31 @@ package logic.elements.characters;
  * @author Erik
  */
 public class Player {
+    private Item[] inventory;
     
+    public Item[] getInventory()
+    {
+        int itemCount = 0;
+        
+        for(Item slot : inventory)
+        {
+            if(slot != null) itemCount++;
+        }
+        
+        if(itemCount == 0) return(new Item[0]);
+        
+        Item[] croppedInventory = new Item[itemCount];
+        
+        int currentSlot = 0;
+        
+        for(Item slot : inventory)
+        {
+            if(slot != null)
+            {
+                croppedInventory[currentSlot++] = slot;
+            }
+        }
+        
+        return(croppedInventory);
+    }
 }
