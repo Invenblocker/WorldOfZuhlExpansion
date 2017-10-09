7eff86b7d0b64f25be1e6fd4fb5b66e8ba6ca992
Loc Hoang Thanh Nguyen <vLocNguyen@live.dk>
2017-10-09 15:23:15 +0200

    ItemRoomUpdate

--- Slower Than Light/src/logic/elements/rooms/ItemRoom.java
+++ Slower Than Light/src/logic/elements/rooms/ItemRoom.java
@@ -1,3 +1,5 @@
+package logic.elements.rooms;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,41 +12,47 @@ import logic.elements.rooms.Room;
  *
  * @author Erik
  */
-public class ItemRoom extends Room{
-    private Item item;
-    private Item specialItem;
+public class ItemRoom extends Room{ //Itemroom inherits funnctionality from room
+    private Item item;              //Items from class player.
+    private Item specialItem;   
     private String repairItem;
 
+    
+    /*
+    Constructor for Itemroom, which extends/inherits functionality from room.
+    */
     public ItemRoom(String name, String repairItem) {
-        super(name, false);
+      super(name, false);//extends room, uses name, and false for cr, as itemroom can't be controlroom
     }
     
-    public void setItem(Item item) 
+    
+     //functionality
+    public void setItem(Item item)                //Sets item in room class
     {
-        this.item = item;
+        this.item = item;          
     }
-     public void setSpecialItem(Item specialItem) 
+     public void setSpecialItem(Item specialItem) //Sets specialItem in room class
     {
         this.specialItem = specialItem;
     }
      
-     public void takeItem(Item item){
+     public void takeItem(Item item){             //removes item from room when taken by player
          this.item = null;
      }
      
-     public void takeSpecialItem(Item item){
+     public void takeSpecialItem(Item item){     //removes item from room when taken by player
          this.specialItem = null;
      }
      
-     public Item getItem(){
+     public Item getItem(){                     //returns item from datatype Item
          return this.item;
      }
      
-      public Item getSpecialItem(){
+      public Item getSpecialItem(){             //returns item from datatype Item
          return this.specialItem;
      }
      
-      public String getRepairItem(){
+      public String getRepairItem(){            //returns item from datatype String
           return this.repairItem;
       }
 }
