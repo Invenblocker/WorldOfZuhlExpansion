92bc3346257f911691364ce34a9acafbb2af665a
JN97 <32257432+JN97@users.noreply.github.com>
2017-10-09 11:23:14 +0200

    Tilføjet Saboteur med constructor

---
+++ Slower Than Light/src/logic/elements/characters/Saboteur.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic.elements.characters;
+
+import logic.elements.rooms.*;
+/**
+ *
+ * @author Johnn
+ */
+public class Saboteur extends RoomHopper {
+    
+    Saboteur(Room room)
+    {
+        super(room);
+        
+    }
+  
+}
