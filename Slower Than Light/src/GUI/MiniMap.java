081fd4b4a2b957bb2d702be3d2fa3a26cd659eec
Loc Hoang Thanh Nguyen <vLocNguyen@live.dk>
2017-10-30 12:00:11 +0100

    MiniMap and GUI updated
    added methods

--- Slower Than Light/src/GUI/MiniMap.java
+++ Slower Than Light/src/GUI/MiniMap.java
@@ -5,6 +5,8 @@
  */
 package GUI;
 
+import java.util.ArrayList;
+import logic.Game;
 import logic.elements.rooms.Room;
 
 /**
@@ -12,16 +14,43 @@ import logic.elements.rooms.Room;
  * @author Peter
  */
 public class MiniMap {
-    
+   private ArrayList<Room> DestroyedRoomsInMiniMap;  //Creates arrayList that stores destroyed rooms  
    public MiniMap(){
        
    }
  
-public void update(){
-    
+public void update(Room saboteurRoom, Room[] destroyedRoomsIngame){                               
+    System.out.print("Updated saboteur position: ");      //updates saboteur position to minimap
+    System.out.println(saboteurRoom); 
+    updateDestroyedRooms(destroyedRoomsIngame);           //calls method, updates destroyedrooms in Game
+    System.out.println("Destroyed rooms in game: ");
 }
 public void updatePlayerPosition(Room room){
-    
+    System.out.print("Current player position: ");        //updates player position to minimap
+    System.out.println(Game.getInstance().getPlayer().getCurrentRoom());
 }
+
+private void updateDestroyedRooms(Room[] destroyedRoomsInGame) {   //Sets param destroydRoomsInGame
+    for (Room destroyedRoom : destroyedRoomsInGame) 
+        if(!DestroyedRoomsInMiniMap.contains(destroyedRoom)) {      //only unique elements, only add if !not already
+            DestroyedRoomsInMiniMap.add(destroyedRoom);             //contains room DestroyedRoomsInMiniMap 
+            
+            
+   }
+    
+
+    
+}    
+    
+    
+            
+        
+    
+    
+    
+    
+
+        
+
     
 }
