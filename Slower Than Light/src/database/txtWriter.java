033988cbff15f25cae61ad4c5b86cbfee7a4a8f6
Loc Hoang Thanh Nguyen <vLocNguyen@live.dk>
2017-11-06 13:41:55 +0100

    txtWriter

---
+++ Slower Than Light/src/database/txtWriter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.util.HashMap;
+import logic.elements.characters.Item;
+import logic.elements.characters.Player;
+import logic.elements.characters.Saboteur;
+import logic.elements.rooms.Room;
+
+/**
+ *
+ * @author Loc
+ */
+public class txtWriter {
+    
+  public void saveGame (HashMap<String, Room> rooms, HashMap<String, Item> items, Player player, Saboteur saboteur, 
+            Helper helper, int roomsRepaired) {
+        
+    }
+     
+   public void writeHighScore(HashMap<String, Integer> highScore) {
+       
+   }
+    
+    
+}
