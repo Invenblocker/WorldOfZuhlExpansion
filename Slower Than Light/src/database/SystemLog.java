a714b058ffecf94223167eef0359c8d2b64702e4
JN97 <32257432+JN97@users.noreply.github.com>
2017-11-07 12:54:09 +0100

    Finished the SystemLog class

--- Slower Than Light/src/database/SystemLog.java
+++ Slower Than Light/src/database/SystemLog.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
  */
 public class SystemLog
 {
-    static ArrayList<String> globalLog = new ArrayList();
-    ArrayList<String> log;
-    String name;
+    private static ArrayList<String> globalLog = new ArrayList();
+    private ArrayList<String> log;
+    private String name;
     
     private static SystemLog errorLog = new SystemLog("Error Log");
     private static SystemLog actionLog = new SystemLog("Action Log");
@@ -26,5 +26,94 @@ public class SystemLog
         log = new ArrayList();
     }
     
+    public void writeToLog(String... message)
+    {
+        for(int i = 0; i < message.length; i++)
+        {
+            log.add(message[i]);
+        }
+        
+        writeToGlobalLog(message);
+    }
     
+    private void writeToGlobalLog(String... message)
+    {
+        for(int i = 0; i < message.length; i++)
+        {
+            globalLog.add(getName() + ": " + message[i]);
+        }
+    }
+    
+    public static SystemLog getLog(String logName)
+    {
+        switch(logName)
+        {
+            case "Error Log":
+                return(errorLog);
+            case "Action Log":
+                return(actionLog);
+            default:
+                errorLog.writeToLog("The log \"" + logName + "\" is not a static log in the SystemLog class.");
+                return(null);
+        }
+    }
+    
+    public static void writeToGlobalLog(String caller, String... message)
+    {
+        for(int i = 0; i < message.length; i++)
+        {
+            globalLog.add(caller + ": " + message[i]);
+        }
+    }
+    
+    public String getName()
+    {
+        return(name);
+    }
+    
+    public String[] getLog()
+    {
+        String[] copy = new String[log.size()];
+        for(int i = 0; i < copy.length; i++)
+        {
+            copy[i] = log.get(i);
+        }
+        return(copy);
+    }
+    
+    public String getLogEntry(int index)
+    {
+        if(index >= 0 && index < log.size())
+        {
+            return(log.get(index));
+        }
+        else
+        {
+            errorLog.writeToLog("The index " + index + " is out of bounds in the log " + getName() + '.');
+            return("");
+        }
+    }
+    
+    public static String[] getGlobalLog()
+    {
+        String[] log = new String[globalLog.size()];
+        for(int i = 0; i < globalLog.size(); i++)
+        {
+            log[i] = globalLog.get(i);
+        }
+        return(log);
+    }
+    
+    public static String getGlobalLogEntry(int index)
+    {
+        if(index >= 0 && index < globalLog.size())
+        {
+            return(globalLog.get(index));
+        }
+        else
+        {
+            errorLog.writeToLog("The index " + index + " is out of bounds in the global log.");
+            return("");
+        }
+    }
 }
