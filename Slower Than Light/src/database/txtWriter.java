e116f1582b4dc0528d35c8ffa87f48a9357259af
Erik <erik@hagebjerg.dk>
2017-11-13 12:15:24 +0100

    Try catch i txtwriter

--- Slower Than Light/src/database/txtWriter.java
+++ Slower Than Light/src/database/txtWriter.java
@@ -46,137 +46,149 @@ public class txtWriter {
             return;
         }
         
-        txtWriter.print("Room: ");
-      for(String key : rooms.keySet()){
-          txtWriter.print(key + " ");
-          if(rooms.get(key) instanceof ItemRoom){
-              txtWriter.print("ItemRoom ");
-              txtWriter.print(rooms.get(key).isOperating());
-          }
-          else if(rooms.get(key) instanceof ControlRoom){
-              txtWriter.print("ControlRoom ");
-              txtWriter.print(rooms.get(key).isOperating());
-          }
-          else if(rooms.get(key) instanceof WorkshopRoom){
-              txtWriter.print("WorkshopRoom ");
-              txtWriter.print(rooms.get(key).isOperating());
-          }
-      }
-      txtWriter.println();
-     
-     
-     
-     
-     for(String key : rooms.keySet()){
-         txtWriter.print(key + " ");
-         if(rooms.get(key).getExit("north") != null){
-             txtWriter.print("north ");
-             exit = rooms.get(key).getExit(rooms.get(key)); 
-             txtWriter.print(exit.isOperating());               // prints the boolean value to the txt file
-             txtWriter.print(rooms.get(key).getExit(exit));     // prints the room on the opposing side of the exit to the txt file.
-         } 
-         if(rooms.get(key).getExit("south") != null) {
-             txtWriter.print("south ");
-             exit = rooms.get(key).getExit(rooms.get(key));
-             txtWriter.print(exit.isOperating());
-             txtWriter.print(rooms.get(key).getExit(exit));
-         }
-         if(rooms.get(key).getExit("west") != null) {
-             txtWriter.print("west ");
-             exit = rooms.get(key).getExit(rooms.get(key));
-             txtWriter.print(exit.isOperating());
-             txtWriter.print(rooms.get(key).getExit(exit));
-         }
-         if(rooms.get(key).getExit("east") != null) {
-             txtWriter.print("east "); // direction
-             exit = rooms.get(key).getExit(rooms.get(key)); 
-             txtWriter.print(exit.isOperating()); // isOperating
-             txtWriter.print(rooms.get(key).getExit(exit)); // Room connected
-         }
-         txtWriter.println();
-         
-     }
-     
-    txtWriter.print("Item: ");
-     for(String key : items.keySet())
-     {
-     txtWriter.print("Tool: ");
-     txtWriter.print(key + " ");
-     txtWriter.print(items.get(key).getDefaultRoom().toString() + " ");
-     }
-     
-    txtWriter.println();
-      
-    txtWriter.print("SpecialItem: ");
-     for(String key : items.keySet())
-     {
-     txtWriter.print("Tool: ");
-     txtWriter.print(key + " ");
-     }
-     
-     txtWriter.println();
-      
         
+        txtWriter.print("Room: ");
+        for(String key : rooms.keySet()){
+            txtWriter.print(key + " ");
+            if(rooms.get(key) instanceof ItemRoom){
+                txtWriter.print("ItemRoom ");
+                txtWriter.print(rooms.get(key).isOperating());
+            }
+            else if(rooms.get(key) instanceof ControlRoom){
+                txtWriter.print("ControlRoom ");
+                txtWriter.print(rooms.get(key).isOperating());
+            }
+            else if(rooms.get(key) instanceof WorkshopRoom){
+                txtWriter.print("WorkshopRoom ");
+                txtWriter.print(rooms.get(key).isOperating());
+            }
+        }
+        txtWriter.println();
+        
+        
+        for(String key : rooms.keySet()){
+            txtWriter.print(key + " ");
+            if(rooms.get(key).getExit("north") != null){
+                txtWriter.print("north ");
+                exit = rooms.get(key).getExit(rooms.get(key)); 
+                txtWriter.print(exit.isOperating());               // prints the boolean value to the txt file
+                txtWriter.print(rooms.get(key).getExit(exit));     // prints the room on the opposing side of the exit to the txt file.
+            } 
+            if(rooms.get(key).getExit("south") != null) {
+                txtWriter.print("south ");
+                exit = rooms.get(key).getExit(rooms.get(key));
+                txtWriter.print(exit.isOperating());
+                txtWriter.print(rooms.get(key).getExit(exit));
+            }
+            if(rooms.get(key).getExit("west") != null) {
+                txtWriter.print("west ");
+                exit = rooms.get(key).getExit(rooms.get(key));
+                txtWriter.print(exit.isOperating());
+                txtWriter.print(rooms.get(key).getExit(exit));
+            }
+            if(rooms.get(key).getExit("east") != null) {
+                txtWriter.print("east "); // direction
+                exit = rooms.get(key).getExit(rooms.get(key)); 
+                txtWriter.print(exit.isOperating()); // isOperating
+                txtWriter.print(rooms.get(key).getExit(exit)); // Room connected
+            }
+            txtWriter.println();
+        }
      
-     txtWriter.print("Player: ");
-     txtWriter.print(player.getCurrentRoom().getName() + " "); //players current room name
-     txtWriter.print(2);                                       //Inventory size
-     
-     Item[] itemArray = player.getInventory();              //Item array that holds players current inventory
-     int i = player.getItemCount();                         //itemcount of how many items player current holds
-     int j = 0;
-     while (j < i){
-      txtWriter.print(itemArray[j].getName() + " ");       //writes the current items into the txtfile
-      txtWriter.print(itemArray[j].getDefaultRoom().getName() + " "); //writes name of current rooms items are stored in
-      j++;
-     }
-     txtWriter.println();
-     
-    txtWriter.print("Saboteur: ");
-    txtWriter.print(saboteur.getCurrentRoom().getName() + " ");
-    txtWriter.print(saboteur.DEFAULT_CHANCE_OF_SABOTAGE + " ");
-    txtWriter.print(saboteur.CHANCE_OF_SABOTAGE_GROWTH + " ");
-    txtWriter.print(saboteur.DEFAULT_CHANCE_OF_SABOTAGE + " ");
-    txtWriter.print(saboteur.getChanceOfSabotage() + " ");
-    txtWriter.print(saboteur.getStunCountdown());
-    txtWriter.println();
-    
-    txtWriter.print("Helper: ");
-    txtWriter.print(helper.getCurrentRoom().getName() + " ");
-    txtWriter.print(helper.getHelperTask().name());
-    txtWriter.print(helper.getHelperTask());
-    txtWriter.print(helper.CHANCE_OF_DISCOVERY_GROWTH);
-    txtWriter.print(helper.DEFAULT_CHANCE_OF_DISCOVERY);
-    txtWriter.println();
-    
-    txtWriter.print("RoomsRepaired: " + roomsRepaired);   // roomsrepaired kommer fra gameinfo.increment 
-    txtWriter.println();
-    
-    txtWriter.print("HelperCountDown: " + time.getHelperCountdown());
-    txtWriter.println();
-    
-    txtWriter.print("Time: " + time.getTimeLeft());
-    txtWriter.println();
-    
-    txtWriter.print("OxygenLeft: " + time.getOxygenLeft());
-    txtWriter.println();
+        txtWriter.print("Item: ");
+        for(String key : items.keySet())
+        {
+        txtWriter.print("Tool: ");
+        txtWriter.print(key + " ");
+        txtWriter.print(items.get(key).getDefaultRoom().toString() + " ");
+        }
 
-    txtWriter.close();
+        txtWriter.println();
+
+        txtWriter.print("SpecialItem: ");
+        for(String key : items.keySet())
+        {
+        txtWriter.print("Tool: ");
+        txtWriter.print(key + " ");
+        }
+
+        txtWriter.println();
+        
+        
+        txtWriter.print("Player: ");
+        txtWriter.print(player.getCurrentRoom().getName() + " "); //players current room name
+        txtWriter.print(2);                                       //Inventory size
+
+        Item[] itemArray = player.getInventory();              //Item array that holds players current inventory
+        int i = player.getItemCount();                         //itemcount of how many items player current holds
+        int j = 0;
+        while (j < i)
+        {
+            txtWriter.print(itemArray[j].getName() + " ");       //writes the current items into the txtfile
+            txtWriter.print(itemArray[j].getDefaultRoom().getName() + " "); //writes name of current rooms items are stored in
+            j++;
+        }
+        txtWriter.println();
      
-     
+        txtWriter.print("Saboteur: ");
+        txtWriter.print(saboteur.getCurrentRoom().getName() + " ");
+        txtWriter.print(saboteur.DEFAULT_CHANCE_OF_SABOTAGE + " ");
+        txtWriter.print(saboteur.CHANCE_OF_SABOTAGE_GROWTH + " ");
+        txtWriter.print(saboteur.DEFAULT_CHANCE_OF_SABOTAGE + " ");
+        txtWriter.print(saboteur.getChanceOfSabotage() + " ");
+        txtWriter.print(saboteur.getStunCountdown());
+        txtWriter.println();
+
+        txtWriter.print("Helper: ");
+        txtWriter.print(helper.getCurrentRoom().getName() + " ");
+        txtWriter.print(helper.getHelperTask().name());
+        txtWriter.print(helper.getHelperTask());
+        txtWriter.print(helper.CHANCE_OF_DISCOVERY_GROWTH);
+        txtWriter.print(helper.DEFAULT_CHANCE_OF_DISCOVERY);
+        txtWriter.println();
+
+        txtWriter.print("RoomsRepaired: " + roomsRepaired);   // roomsrepaired kommer fra gameinfo.increment 
+        txtWriter.println();
+
+        txtWriter.print("HelperCountDown: " + time.getHelperCountdown());
+        txtWriter.println();
+
+        txtWriter.print("Time: " + time.getTimeLeft());
+        txtWriter.println();
+
+        txtWriter.print("OxygenLeft: " + time.getOxygenLeft());
+        txtWriter.println();
+
+        txtWriter.close();
+
+
   }
      
-   public static void writeHighScore(LinkedHashMap<String, Integer> highScore, String highscoreName) throws FileNotFoundException {
-       // hashmappet må kun være en vis størrelse
-     File newSave = new File(highscoreName);
-     PrintWriter txtWriter = new PrintWriter(highscoreName);
-     for (String key : highScore.keySet()){
-         txtWriter.println(key + highScore.get(key + " "));
-         
-     }
-     txtWriter.close();
-       
-   }
-    
+   public static void writeHighScore(LinkedHashMap<String, Integer> highScore, String highscoreName)
+   {
+        // hashmappet må kun være en vis størrelse
+        File newSave = new File(highscoreName);
+        PrintWriter txtWriter = null;
+        
+        try
+        {
+            txtWriter = new PrintWriter(highscoreName);
+        }
+        catch (FileNotFoundException e)
+        {
+            String msg = "Cannot setup the write file in txtWriter";
+            SystemLog.getErrorLog().writeToLog(msg);
+            System.out.println(msg + "\n" + e.getMessage());
+            return;
+        }
+     
+     
+        for (String key : highScore.keySet())
+        {
+            txtWriter.println(key + highScore.get(key + " "));
+        }
+        
+        txtWriter.close();
+    }
     
 }
