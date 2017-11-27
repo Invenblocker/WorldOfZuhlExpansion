b433efc925c50957a2b548b2e68a92702a31efeb
Pottemuld <sdowning@live.dk>
2017-11-27 12:37:01 +0100

    Main menu oprettet

---
+++ Slower Than Light/src/GUI/MainMenuController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author sdown
+ */
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private Button newGameButton;
+    @FXML
+    private Button loadGameButton;
+    @FXML
+    private Button highscoreButton;
+    @FXML
+    private Button quitButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    
+    
+    }    
+
+   
+    
+    
+}
