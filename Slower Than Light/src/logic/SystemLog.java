0783bc4b8ce903af9c24b4aefd638ff93fdb5c7c
Invenblocker <32257137+Invenblocker@users.noreply.github.com>
2017-12-13 14:49:36 +0100

    Added JavaDoc to SystemLog

--- Slower Than Light/src/logic/SystemLog.java
+++ Slower Than Light/src/logic/SystemLog.java
@@ -11,8 +11,8 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
- *
- * @author Johnn
+ * A class for storing logs that can be saved as txt files when the execution ends.
+ * @author Invenblocker
  */
 public class SystemLog
 {
@@ -20,12 +20,17 @@ public class SystemLog
     private final static ArrayList<String> GLOBAL_LOG = new ArrayList();
     private final static SystemLog ERROR_LOG = new SystemLog("Error Log");
     private final static SystemLog ACTION_LOG = new SystemLog("Action Log");
-    private final static String logPath = "Logs/";
+    private final static String LOG_PATH = "Logs/";
     
     private final ArrayList<String> LOG;
     private final String NAME;
     private final SystemLog PARENT_LOG;
     
+    /**
+     * Creates a SystemLog referring to the GLOBAL_LOG as its parent.
+     * @author Invenblocker
+     * @param name The name of the log.
+     */
     public SystemLog(String name)
     {
         this.NAME = name;
@@ -34,6 +39,12 @@ public class SystemLog
         SYSTEM_LOGS.add(this);
     }
     
+    /**
+     * Creates a SystemLog with a preference to a parentLog.
+     * @author Invenblocker
+     * @param name The name of the log.
+     * @param parentLog The log to be used as a parent.
+     */
     public SystemLog(String name, SystemLog parentLog)
     {
         this.NAME = name;
@@ -42,6 +53,13 @@ public class SystemLog
         SYSTEM_LOGS.add(this);
     }
     
+    /**
+     * Writes a message to the log instance. Message is forwarded to parent log
+     * and continues geting forwarded until it eventually ends up in the
+     * GLOBAL_LOG.
+     * @author Invenblocker
+     * @param message The message(s) to write in the log.
+     */
     public void writeToLog(String... message)
     {
         for(int i = 0; i < message.length; i++)
@@ -51,6 +69,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Used by logs to write into their parentLog.
+     * @author Invenblocker
+     * @param message The message to forward.
+     */
     private void writeToParentLog(String message)
     {
         if(PARENT_LOG != null)
@@ -63,6 +86,12 @@ public class SystemLog
         }
     }
     
+    /**
+     * Allows fetching of one of the two static logs through a String parameter.
+     * @author Invenblocker
+     * @param logName The name of the log in question.
+     * @return The log that the program has attempted to fetch.
+     */
     public static SystemLog getLog(String logName)
     {
         switch(logName)
@@ -77,6 +106,12 @@ public class SystemLog
         }
     }
     
+    /**
+     * Writes into the GLOBAL_LOG.
+     * @author Invenblocker
+     * @param caller The name of the caller that writes to the GLOBAL_LOG.
+     * @param message The message(s) that should be written in the GLOBAL_LOG.
+     */
     public static void writeToGlobalLog(String caller, String... message)
     {
         for(int i = 0; i < message.length; i++)
@@ -85,11 +120,20 @@ public class SystemLog
         }
     }
     
+    /**
+     * @author Invenblocker
+     * @return The name of the Log.
+     */
     public String getName()
     {
         return(NAME);
     }
     
+    /**
+     * Creates an array containing the messages saved in the log.
+     * @author Invenblocker
+     * @return The messages saved in the log.
+     */
     public String[] getLog()
     {
         String[] copy = new String[LOG.size()];
@@ -100,6 +144,12 @@ public class SystemLog
         return(copy);
     }
     
+    /**
+     * Fetches the log entry at a given index.
+     * @author Invenblocker
+     * @param index The index an entry should be fetched from.
+     * @return The message at the given index.
+     */
     public String getLogEntry(int index)
     {
         if(index >= 0 && index < LOG.size())
@@ -113,6 +163,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Creates an array containing the messages saved in the GLOBAL_LOG.
+     * @author Invenblocker
+     * @return The messages saved in the GLOBAL_LOG.
+     */
     public static String[] getGlobalLog()
     {
         String[] log = new String[GLOBAL_LOG.size()];
@@ -123,6 +178,12 @@ public class SystemLog
         return(log);
     }
     
+    /**
+     * Fetches the log entry at a given index in the GLOBAL_LOG.
+     * @author Invenblocker
+     * @param index The index an entry should be fetched from.
+     * @return The message at the given index.
+     */
     public static String getGlobalLogEntry(int index)
     {
         if(index >= 0 && index < GLOBAL_LOG.size())
@@ -136,16 +197,28 @@ public class SystemLog
         }
     }
     
+    /**
+     * @author Invenblocker
+     * @return The static ERROR_LOG.
+     */
     public static SystemLog getErrorLog()
     {
         return(ERROR_LOG);
     }
     
+    /**
+     * @author Invenblocker
+     * @return the static ACTION_LOG.
+     */
     public static SystemLog getActionLog()
     {
         return(ACTION_LOG);
     }
     
+    /**
+     * Prints the contents of GLOBAL_LOG to the console.
+     * @author Invenblocker
+     */
     public static void printGlobalLog()
     {
         String[] log = getGlobalLog();
@@ -155,6 +228,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the entry at a given GLOBAL_LOG index to the console.
+     * @author Invenblocker
+     * @param index The index of the entry to be printed.
+     */
     public static void printGlobalLogIndex(int... index)
     {
         String[] log = getGlobalLog();
@@ -167,6 +245,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of GLOBAL_LOG from a given index.
+     * @author Invenblocker
+     * @param index The index of the starting entry.
+     */
     public static void printGlobalLogFrom(int index)
     {
         String[] log = getGlobalLog();
@@ -176,6 +259,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of GLOBAL_LOG to (not including) a given index.
+     * @author Invenblocker
+     * @param index The index of the ending entry.
+     */
     public static void printGlobalLogTo(int index)
     {
         String[] log = getGlobalLog();
@@ -185,6 +273,12 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of GLOBAL_LOG between two given indexes.
+     * @author Invenblocker
+     * @param a The starting index.
+     * @param b The ending index.
+     */
     public static void printGlobalLogRange(int a, int b)
     {
         String[] log = getGlobalLog();
@@ -194,6 +288,10 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the content of the log to the console.
+     * @author Invenblocker
+     */
     public void printLog()
     {
         String[] log = getLog();
@@ -203,6 +301,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the entry at a given index in the log.
+     * @author Invenblocker
+     * @param index The index of the entry to print.
+     */
     public void printLogIndex(int... index)
     {
         String[] log = getLog();
@@ -215,6 +318,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of the log from the given index.
+     * @author Invenblocker
+     * @param index The starting index.
+     */
     public void printLogFrom(int index)
     {
         String[] log = getLog();
@@ -224,6 +332,11 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of the log to (not including) the given index.
+     * @author Invenblocker
+     * @param index The ending index.
+     */
     public void printLogTo(int index)
     {
         String[] log = getLog();
@@ -233,6 +346,12 @@ public class SystemLog
         }
     }
     
+    /**
+     * Prints the contents of the log between two given indexes.
+     * @author Invenblocker
+     * @param a The starting index.
+     * @param b The ending index.
+     */
     public void printLogRange(int a, int b)
     {
         String[] log = getLog();
@@ -242,11 +361,16 @@ public class SystemLog
         }
     }
     
+    /**
+     * Saves the GlobalLog as a .txt file.
+     * @author Invenblocker
+     * @param name The name of the .txt file (excluding fileType extension).
+     */
     public static void saveGlobalLog(String name)
     {
         try
         {
-            File globalLog = new File(logPath + fileNamePurge(name) + ".txt");
+            File globalLog = new File(LOG_PATH + fileNamePurge(name) + ".txt");
             PrintWriter logWriter = new PrintWriter(globalLog);
 
             String[] log = getGlobalLog();
@@ -265,11 +389,15 @@ public class SystemLog
         }
     }
     
+    /**
+     * Saves the log as a .txt file.
+     * @author Invenblocker
+     */
     public void saveLog()
     {
         try
         {
-            File currentLog = new File(logPath + fileNamePurge(getLongName()) + ".txt");
+            File currentLog = new File(LOG_PATH + fileNamePurge(getLongName()) + ".txt");
             PrintWriter logWriter = new PrintWriter(currentLog);
 
             String[] log = getLog();
@@ -288,6 +416,10 @@ public class SystemLog
         }
     }
     
+    /**
+     * Saves all logs as .txt files.
+     * @author Invenblocker
+     */
     public static void saveAllLogs()
     {
         System.out.println("SAVING ALL LOGS");
@@ -298,11 +430,17 @@ public class SystemLog
         saveGlobalLog("GlobalLog");
     }
     
+    /**
+     * Finds the long name of a log based on its chain of parent logs leading
+     * back to the GLOBAL_LOG.
+     * @author Invenblocker
+     * @return The long name of a log.
+     */
     public String getLongName()
     {
         if(PARENT_LOG != null)
         {
-            return(PARENT_LOG.getLongName() + " - " + getName());
+            return(PARENT_LOG.getLongName() + ": " + getName());
         }
         else
         {
@@ -310,11 +448,20 @@ public class SystemLog
         }
     }
     
+    /**
+     * Removes all contents from a log.
+     * @author Invenblocker
+     */
     public void wipeLog()
     {
         LOG.clear();
     }
     
+    /**
+     * Wipes the GLOBAL_LOG and resets the SYSTEM_LOGS list as well as the
+     * ACTION_LOG and ERROR_LOG.
+     * @author Invenblocker
+     */
     public static void wipeAllLogs()
     {
         SYSTEM_LOGS.clear();
@@ -325,6 +472,11 @@ public class SystemLog
         SYSTEM_LOGS.add(ERROR_LOG);
     }
     
+    /**
+     * Removes symbols that aren't allowed in file names.
+     * @param text The String to purge.
+     * @return The string after invalid symbols have been removed.
+     */
     private static String fileNamePurge(String text)
     {
         text = text.replace('~', '-');
